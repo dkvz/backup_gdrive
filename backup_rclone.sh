e90bd8c0f5c2e5c2caceab935706463da38ddd07
DkVZ <github@dkvz.eu>
2025-03-07 20:26:19 +0100

    add a simple replacement script using rclone

---
+++ backup_rclone.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BACKUP_SOURCES="/srv/
+/etc/nginx/
+/etc/nftables.conf
+/etc/postfix/
+/home/mailuser/"
+
+# <RCLONE_REMOTE>:<BACKUP_DIR>
+# No trailing slash!
+RCLONE_DEST="onedrive:/vps"
+
+for s in $BACKUP_SOURCES; do
+  if ! rclone sync "$s" "${RCLONE_DEST}/${s}"
+  then
+    echo "backup sync error for ${s}"
+  fi
+done
